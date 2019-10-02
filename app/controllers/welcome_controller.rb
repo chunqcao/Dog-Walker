3a46b809a52b27283b5f5d88326a988ba212fd37
chop <byao0303@gmail.com>
2019-10-02 00:08:06 -0500

    Set up env

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# welcome page
+class WelcomeController < ApplicationController
+  def index; end
+end
--- db/migrate/20190912024215_create_dogs_events_join_table.rb
+++ db/migrate/20190912024215_create_dogs_events_join_table.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateDogsEventsJoinTable < ActiveRecord::Migration[6.0]
   def change
     create_join_table :dogs, :events do |t|
